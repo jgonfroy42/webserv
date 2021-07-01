6c556ffe1f99f5d572a45e40cf27e7a2946eb437
celeloup <celia.m.leloup@gmail.com>
2021-07-01 11:46:20 +0200

    started bad request tests

--- tests/tester.sh
+++ tests/tester.sh
@@ -1,21 +1,5 @@
 # TESTEUR
 
-NOTES :
-GET /
-sans protocole, marche quand meme (200 OK + index nginx)
-
-GET / h
-404 not found
-
-GET / H 
-400 Bad Request
-
-GET / HTTP
-400 Bad Request
-
-bad header
-il ignore
-
 # Test bad requests
 # Test plusieurs serveurs
 
